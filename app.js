e2c2dbfab141796eff52efbae1ce2820caf5935e
Nadya Rozhkova <nr0644@yandex.ru>
2020-06-12 16:41:49 +0300

    add first version of controllers and routes for accounts and categories

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const accountRouter = require('./routes/accountRoutes');
+const categoryRouter = require('./routes/categoryRoutes');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use('/api/accounts', accountRouter);
+app.use('/api/categories', categoryRouter);
 
 module.exports = app;
---
+++ controllers/accountController.js
@@ -0,0 +1,92 @@
+const Account = require('../models/accountModel');
+
+exports.getAllAccounts = async (req, res) => {
+    try {
+        const accounts = await Account.find();
+        res.status(200).json({
+            status: 'success',
+            results: accounts.length,
+            data: {
+                accounts
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
+};
+
+exports.getAccount = async (req, res) => {
+    try {
+        const account = await Account.findById(+req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                account
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
+};
+
+exports.createAccount = async (req, res) => {
+    try {
+        const newAccount = await Account.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                account: newAccount
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
+};
+
+exports.updateAccount = async (req, res) => {
+    try {
+        const accountToUpdate = await Account.findByIdAndUpdate(
+            +req.params.id,
+            req.body, 
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+        req.status(200).json({
+            status: 'success',
+            data: {
+                accountToUpdate
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
+};
+
+exports.deleteAccount = async (req, res) => {
+    try {
+        await Account.findByIdAndDelete(+req.params.id);
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        }) 
+    }
+}; 
